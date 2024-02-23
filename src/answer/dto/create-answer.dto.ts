70416b770b0f5e442378e1caf48408621333dd46
Alonso Bonilla <luis.labp06@gmail.com>
2024-02-23 12:48:00 -0500

    Add properties to CreateAnswerDto

--- src/answer/dto/create-answer.dto.ts
+++ src/answer/dto/create-answer.dto.ts
@@ -1,1 +1,14 @@
-export class CreateAnswerDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateAnswerDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  answer: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  questionId: number;
+}

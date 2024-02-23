412cc2a7ac9301d3d8050b7f0b110c65135ea0a3
Alonso Bonilla <luis.labp06@gmail.com>
2024-02-23 12:45:57 -0500

    Refactor global pipes and update API version

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   const config = new DocumentBuilder()
     .setTitle('acTeam')
     .setDescription('Initial API')
-    .setVersion('0.1')
+    .setVersion('1.0')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);

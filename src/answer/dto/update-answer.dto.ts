7d5dfb9630b307b1a89665138fe2fb667a4ffe8e
Alonso Bonilla <luis.labp06@gmail.com>
2024-02-23 12:47:46 -0500

    Update import statement in UpdateAnswerDto

--- src/answer/dto/update-answer.dto.ts
+++ src/answer/dto/update-answer.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateAnswerDto } from './create-answer.dto';
 
 export class UpdateAnswerDto extends PartialType(CreateAnswerDto) {}

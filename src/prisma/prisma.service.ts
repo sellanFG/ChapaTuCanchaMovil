3b79c480ba69a7a36ed84dbae5518c170ea325b4
Alonso Bonilla <luis.labp06@gmail.com>
2024-02-25 10:20:52 -0500

    Add error handling to handleDbOperation method

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,21 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  OnModuleInit,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async handleDbOperation(operation: Promise<any>): Promise<any> {
+    try {
+      return await operation;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 }

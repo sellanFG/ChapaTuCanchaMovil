76ef4eff31c35e5072bf214a3584035a8d940c7a
Alonso Bonilla <luis.labp06@gmail.com>
2024-02-25 22:50:43 -0500

    Refactor player.controller.ts

--- src/player/player.controller.ts
+++ src/player/player.controller.ts
@@ -1,8 +1,17 @@
-import { Controller,Get, Post, Put, Delete, Param ,Body,NotFoundException } from '@nestjs/common';
-import { PlayerService } from './player.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { player } from '@prisma/client';
-import { CreatePlayerDto,UpdatePlayerDto } from './dto';
+import { CreatePlayerDto, UpdatePlayerDto } from './dto';
+import { PlayerService } from './player.service';
 
 @ApiTags('player')
 @Controller('player')
@@ -10,31 +19,27 @@ export class PlayerController {
   constructor(private readonly playerService: PlayerService) {}
 
   @Get()
-  async getPlayers(){
+  async getPlayers() {
     return this.playerService.getPlayers();
   }
 
   @Get(':id')
-    async getPlayerById(@Param('id') id: string){
-      const playerFound = await this.playerService.getPlayerById(Number(id))
-      if(!playerFound){
-        throw new NotFoundException(`Player with ID ${id} not found`);
-      }
-      return playerFound;
-    }
-  
+  async getPlayerById(@Param('id') id: string) {
+    return this.playerService.getPlayerById(Number(id));
+  }
+
   @Post()
-    async createPlayer(@Body() data: CreatePlayerDto){
-      return this.playerService.createPlayer(data);
-    }
+  async createPlayer(@Body() data: CreatePlayerDto) {
+    return this.playerService.createPlayer(data);
+  }
 
   @Put(':id')
-    async updatePlayer(@Param('id') id: number, @Body() data: UpdatePlayerDto){
-      return this.playerService.updatePlayer(id, data);
-    }
-  
-  @Delete(':id')              
-    async deletePlayer(@Param('id') id: number){
-      return this.playerService.deletePlayer(id);
-    }
+  async updatePlayer(@Param('id') id: number, @Body() data: UpdatePlayerDto) {
+    return this.playerService.updatePlayer(id, data);
+  }
+
+  @Delete(':id')
+  async deletePlayer(@Param('id') id: number) {
+    return this.playerService.deletePlayer(id);
+  }
 }

6e01bffd8ab6887dbd48feea5ff5a2c2e583071a
Alonso Bonilla <luis.labp06@gmail.com>
2024-02-26 10:20:32 -0500

    Add API endpoint to get all questions for a sport

--- src/template-questions/template-questions.controller.ts
+++ src/template-questions/template-questions.controller.ts
@@ -1,9 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiOkResponse, ApiParam, ApiTags } from '@nestjs/swagger';
+import { TeamplateQuestions } from '@prisma/client';
+import { TemplateQuestions } from './entities/template-questions.entity';
 import { TemplateQuestionsService } from './template-questions.service';
-import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('template-questions')
 @Controller('template-questions')
 export class TemplateQuestionsController {
-  constructor(private readonly templateQuestionsService: TemplateQuestionsService) {}
+  constructor(
+    private readonly templateQuestionsService: TemplateQuestionsService,
+  ) {}
+
+  @Get(':id')
+  @ApiOkResponse({
+    description: 'Get all questions for a sport',
+    type: [TemplateQuestions],
+  })
+  @ApiParam({ name: 'id', description: 'Sport id', type: 'number' })
+  async getAllQuestionsSportId(
+    @Param('id') id: number,
+  ): Promise<TeamplateQuestions[]> {
+    return this.templateQuestionsService.getAllQuestionsSportId(id);
+  }
 }

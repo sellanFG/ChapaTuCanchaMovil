824ccf7a518dc1cd0b07c85081ce48e5ddae75d3
Alonso Bonilla <luis.labp06@gmail.com>
2024-02-26 10:43:50 -0500

    Add SportController to SportModule

--- src/sport/sport.module.ts
+++ src/sport/sport.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { SportController } from './sport.controller';
 import { SportService } from './sport.service';
 
 @Module({
   providers: [SportService],
+  controllers: [SportController],
   exports: [SportService],
   imports: [PrismaModule],
 })

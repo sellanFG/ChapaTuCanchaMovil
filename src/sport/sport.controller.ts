d1ae5e3e185bbb6105571a23358f013522e5b15d
Alonso Bonilla <luis.labp06@gmail.com>
2024-02-26 10:50:55 -0500

    Add GET endpoints for retrieving sports

--- src/sport/sport.controller.ts
+++ src/sport/sport.controller.ts
@@ -1,9 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiParam, ApiTags } from '@nestjs/swagger';
+import { Sport } from './entities/sport.entity';
 import { SportService } from './sport.service';
-import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('sport')
 @Controller('sport')
 export class SportController {
   constructor(private readonly sportService: SportService) {}
+
+  @Get()
+  @ApiOkResponse({ type: [Sport] })
+  getSports() {
+    return this.sportService.getSports();
+  }
+
+  @Get(':id')
+  @ApiOkResponse({ type: [Sport] })
+  @ApiParam({ name: 'id', description: 'Sport id', type: 'number' })
+  getSportById(id: number) {
+    return this.sportService.getSportById(id);
+  }
 }

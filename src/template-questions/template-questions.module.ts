a9bc94bb2b8ad09ee25811dab6e9242c2e250291
Alonso Bonilla <luis.labp06@gmail.com>
2024-02-26 10:18:46 -0500

    Add imports for PrismaModule and SportModule

--- src/template-questions/template-questions.module.ts
+++ src/template-questions/template-questions.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { TemplateQuestionsService } from './template-questions.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { SportModule } from 'src/sport/sport.module';
 import { TemplateQuestionsController } from './template-questions.controller';
+import { TemplateQuestionsService } from './template-questions.service';
 
 @Module({
   controllers: [TemplateQuestionsController],
   providers: [TemplateQuestionsService],
+  imports: [PrismaModule, SportModule],
 })
 export class TemplateQuestionsModule {}

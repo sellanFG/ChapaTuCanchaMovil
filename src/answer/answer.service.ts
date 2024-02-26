b4e0c708fde5e16f1001579d3f9fc719dbf8df9d
Alonso Bonilla <luis.labp06@gmail.com>
2024-02-26 10:42:36 -0500

    Refactor AnswerService to use TemplateQuestionsService

--- src/answer/answer.service.ts
+++ src/answer/answer.service.ts
@@ -1,67 +1,27 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Prisma, answer } from '@prisma/client';
+import { answer } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { CreateAnswerDto, UpdateAnswerDto } from './dto/index';
+import { TemplateQuestionsService } from 'src/template-questions/template-questions.service';
 @Injectable()
 export class AnswerService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private questionService: TemplateQuestionsService,
+  ) {}
 
-  async create(data: CreateAnswerDto): Promise<answer> {
-    const questionId = data.questionId;
-
-    const question = await this.prisma.teamplateQuestions.findUnique({
-      where: {
-        teamplateQuestionsId: questionId,
-      },
-    });
-
-    if (!question) {
-      throw new NotFoundException(`Question with id ${questionId} not found`);
-    }
-
-    return this.prisma.answer.create({
-      data: {
-        answer: data.answer,
-        teamplateQuestionsId: questionId,
-      },
-    });
+  getAll(): Promise<answer[]> {
+    return this.prisma.handleDbOperation(this.prisma.answer.findMany());
   }
 
-  findAll(): Promise<answer[]> {
-    return this.prisma.answer.findMany();
-  }
+  async getAllAnswersQuestionId(questionId: number): Promise<answer[]> {
+    await this.questionService.getById(questionId);
 
-  async findOne(data: Prisma.answerWhereUniqueInput): Promise<answer> {
-    const answer = await this.prisma.answer.findUnique({
-      where: data,
-    });
-
-    if (!answer) {
-      throw new NotFoundException(`Answer with id ${data.answerId} not found`);
-    }
-
-    return answer;
-  }
-
-  async update(id: number, updateAnswerDto: UpdateAnswerDto): Promise<answer> {
-    const { questionId, answer } = updateAnswerDto;
-    console.log(questionId, answer, id);
-    await this.findOne({ answerId: id });
-
-    return this.prisma.answer.update({
-      where: { answerId: id },
-      data: {
-        answer,
-        teamplateQuestionsId: questionId,
-      },
-    });
-  }
-
-  async remove(id: number): Promise<answer> {
-    await this.findOne({ answerId: id });
-
-    return this.prisma.answer.delete({
-      where: { answerId: id },
-    });
+    return this.prisma.handleDbOperation(
+      this.prisma.answer.findMany({
+        where: {
+          teamplateQuestionsId: questionId,
+        },
+      }),
+    );
   }
 }

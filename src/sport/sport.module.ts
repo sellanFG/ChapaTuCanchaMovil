82d86631d4feb676893c90f9eadbd2314178b05a
Alonso Bonilla <luis.labp06@gmail.com>
2024-02-25 23:00:40 -0500

    Add PrismaModule to SportModule and implement new methods in SportService

--- src/sport/sport.module.ts
+++ src/sport/sport.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { SportService } from './sport.service';
-import { SportController } from './sport.controller';
 
 @Module({
-  controllers: [SportController],
   providers: [SportService],
+  exports: [SportService],
+  imports: [PrismaModule],
 })
 export class SportModule {}
--- src/sport/sport.service.ts
+++ src/sport/sport.service.ts
@@ -1,4 +1,58 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Sport } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
-export class SportService {}
+export class SportService {
+  constructor(private prisma: PrismaService) {}
+
+  async getSportById(id: number): Promise<Sport> {
+    const sport = await this.prisma.handleDbOperation(
+      this.prisma.sport.findUnique({
+        where: {
+          SportId: id,
+        },
+      }),
+    );
+
+    if (!sport) {
+      throw new NotFoundException(`Sport with id ${id} not found`);
+    }
+
+    return sport;
+  }
+
+  async validateSports(ids: number[]): Promise<number[]> {
+    const foundSports = await this.prisma.handleDbOperation(
+      this.prisma.sport.findMany({
+        where: {
+          SportId: {
+            in: ids,
+          },
+        },
+      }),
+    );
+
+    const badIds = [];
+    if (foundSports.length != ids.length) {
+      let i = 0,
+        j = 0;
+      while (i < foundSports.length && j < ids.length) {
+        const sportId = (await foundSports[i]).SportId;
+        const id = ids[j];
+
+        if (sportId == id) {
+          i++;
+          j++;
+        } else {
+          badIds.push(id);
+          j++;
+        }
+      }
+
+      for (let i = j; i < ids.length; i++) {
+        badIds.push(ids[i]);
+      }
+    }
+    return badIds;
+  }
+}

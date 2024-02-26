c87de52aef6357430ec1657adb64ef6bf666bbf9
Alonso Bonilla <luis.labp06@gmail.com>
2024-02-26 17:18:17 -0500

    Add status in sportPlayer

---
+++ prisma/migrations/20240226170656_drop_status_preferences/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `status` on the `preferences` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "preferences" DROP COLUMN "status";
---
+++ prisma/migrations/20240226171104_add_status_column/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "sportPlayer" ADD COLUMN     "status" BOOLEAN NOT NULL DEFAULT true;

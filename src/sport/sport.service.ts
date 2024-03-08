052a12df57ad13e2c4bc2d60796cdc9890094759
Alonso Bonilla <luis.labp06@gmail.com>
2024-03-07 19:52:01 -0500

    remove handle function

--- src/sport/sport.service.ts
+++ src/sport/sport.service.ts
@@ -7,13 +7,11 @@ export class SportService {
   constructor(private prisma: PrismaService) {}
 
   async getSportById(id: number): Promise<Sport> {
-    const sport = await this.prisma.handleDbOperation(
-      this.prisma.sport.findUnique({
-        where: {
-          sportId: id,
-        },
-      }),
-    );
+    const sport = await this.prisma.sport.findUnique({
+      where: {
+        sportId: id,
+      },
+    });
 
     if (!sport) {
       throw new NotFoundException(`Sport with id ${id} not found`);
@@ -23,24 +21,26 @@ export class SportService {
   }
 
   async validateSports(ids: number[]): Promise<void> {
-    const foundSports = await this.prisma.handleDbOperation(
-      this.prisma.sport.findMany({
+    try {
+      const foundSports = await this.prisma.sport.findMany({
         where: {
           sportId: {
             in: ids,
           },
         },
-      }),
-    );
+      });
 
-    const foundIds = new Set(foundSports.map((sport) => sport.sportId));
-    const badIds = ids.filter((id) => !foundIds.has(id));
-    if (badIds.length != 0) {
-      throw new NotFoundException(`Invalid ids: ${badIds.join(', ')}`);
+      const foundIds = new Set(foundSports.map((sport) => sport.sportId));
+      const badIds = ids.filter((id) => !foundIds.has(id));
+      if (badIds.length != 0) {
+        throw new NotFoundException(`Invalid ids: ${badIds.join(', ')}`);
+      }
+    } catch (error) {
+      throw new Error(error);
     }
   }
 
   async getSports(): Promise<Sport[]> {
-    return this.prisma.handleDbOperation(this.prisma.sport.findMany());
+    return this.prisma.sport.findMany();
   }
 }

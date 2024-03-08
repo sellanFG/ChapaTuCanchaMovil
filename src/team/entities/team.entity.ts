242a9fa2a46260a066413efa0d61aeed4849b340
Alonso Bonilla <luis.labp06@gmail.com>
2024-03-08 17:08:25 -0500

    Refactor team entity class

--- src/team/entities/team.entity.ts
+++ src/team/entities/team.entity.ts
@@ -1,22 +1,22 @@
-import {ApiProperty} from '@nestjs/swagger'
+import { ApiProperty } from '@nestjs/swagger';
 
-export class team {
-    @ApiProperty()
-    teamId: number;
-
-    @ApiProperty()
-    teamName: string;
-
-    @ApiProperty()
-    teamLogo: string;
-
-    @ApiProperty()
-    teamRegistrationDate: Date;
-
-    @ApiProperty()
-    teamSearchStatus: string;
-
-    @ApiProperty()
-    SportId: number;
+export class TeamEntity {
+  constructor(
+    teamName: string,
+    teamLogo: string,
+    teamSearchStatus: string,
+    sportId: number,
+    teamRegistrationDate: Date,
+  ) {
+    this.teamName = teamName;
+    this.teamLogo = teamLogo;
+    this.teamSearchStatus = teamSearchStatus;
+    this.sportId = sportId;
+    this.teamRegistrationDate = teamRegistrationDate;
+  }
+  teamName: string;
+  teamLogo: string;
+  teamRegistrationDate: Date;
+  teamSearchStatus: string;
+  sportId: number;
 }
-    

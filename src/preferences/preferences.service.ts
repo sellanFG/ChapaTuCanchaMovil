112816cbceeb95ee05fbb5f57c46ffbd61b66d48
Alonso Bonilla <luis.labp06@gmail.com>
2024-03-11 22:47:28 -0500

    refactor preferences service

--- src/preferences/preferences.service.ts
+++ src/preferences/preferences.service.ts
@@ -1,96 +1,93 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreatePreferenceDto } from './dto/createPreferenceDto.dto';
+import { PreferencesEntity } from './entities/preferences.entity';
+import {
+  ArrayPreferences,
+  PreferencesInterface,
+} from './interfaces/preferences.interface';
 
 @Injectable()
 export class PreferencesService {
   constructor(private prisma: PrismaService) {}
 
-  async create(data: CreatePreferenceDto): Promise<NonNullable<unknown>> {
-    const { playerId, sportId, answerId } = data;
+  async create(data: CreatePreferenceDto): Promise<PreferencesInterface[]> {
+    const { playerId, answerIds } = data;
+    const preferences = answerIds.map(
+      (answerId) => new PreferencesEntity(answerId, playerId),
+    );
     await this.prisma.handleDbOperation(
-      this.prisma.preferences.create({
-        data,
+      this.prisma.preferences.createMany({
+        data: preferences,
       }),
     );
-    return this.getPreferences(playerId, sportId, answerId);
+    return this.getPreferences(playerId, answerIds);
   }
 
   async getPreferences(
     playerId: number,
-    sportId?: number,
-    answerId?: number,
-  ): Promise<NonNullable<unknown>> {
-    const answers = await this.prisma.handleDbOperation(
-      this.prisma.preferences.findMany({
-        where: {
-          playerId,
-          sportId,
-          answerId,
-          SportPlayer: {
-            status: true,
+    answerIds?: number[],
+  ): Promise<PreferencesInterface[]> {
+    const preferences = await this.prisma.preferences.findMany({
+      where: {
+        playerId,
+        Answer: {
+          answerId: {
+            in: answerIds,
           },
         },
-        include: {
-          Answer: {
-            include: {
-              Question: true,
-            },
-          },
-          SportPlayer: {
-            include: {
-              Sport: true,
+      },
+      include: {
+        Answer: {
+          include: {
+            Question: {
+              include: {
+                Sport: true,
+              },
             },
           },
         },
-      }),
-    );
-    return this.buildPreferencesModel(answers);
+      },
+    });
+    return this.buildPreferencesModel(preferences);
   }
 
-  private buildPreferencesModel(answers: any): NonNullable<unknown> {
-    const preferences = {};
-    answers.forEach((answer) => {
-      const sportName = answer.SportPlayer.Sport.sportName;
-      if (!preferences[sportName]) {
-        preferences[sportName] = {
-          sportId: answer.sportId,
-          sportImage: answer.SportPlayer.Sport.sportImage,
+  private buildPreferencesModel(preferences: any): PreferencesInterface[] {
+    const model: Record<string, any> = {};
+    const sportNames = [];
+    preferences.forEach((preference) => {
+      const sportName = preference.Answer.Question.Sport.sportName;
+      if (!model[sportName]) {
+        model[sportName] = <PreferencesInterface>{
+          sportId: preference.Answer.Question.sportId,
+          sportName: preference.Answer.Question.Sport.sportName,
+          sportImage: preference.Answer.Question.Sport.sportImage,
           preferences: [],
         };
+        sportNames.push(sportName);
       }
 
-      preferences[sportName]['preferences'].push({
-        questionId: answer.Answer.questionsTemplateId,
-        question: answer.Answer.Question.question,
-        answerId: answer.answerId,
-        answer: answer.Answer.answer,
+      model[sportName]['preferences'].push(<ArrayPreferences>{
+        questionId: preference.Answer.questionsTemplateId,
+        question: preference.Answer.Question.question,
+        answerId: preference.answerId,
+        answer: preference.Answer.answer,
       });
     });
-    return preferences;
+
+    return sportNames.map((sportName) => model[sportName]);
   }
 
-  async delete(
-    playerId: number,
-    sportId: number,
-    answerId: number,
-  ): Promise<NonNullable<unknown>> {
-    const deletePreference = await this.getPreferences(
-      playerId,
-      sportId,
-      answerId,
-    );
+  async delete(playerId: number, answerId: number): Promise<void> {
     await this.prisma.handleDbOperation(
       this.prisma.preferences.delete({
         where: {
-          playerId_sportId_answerId: {
+          playerId_answerId: {
             playerId,
-            sportId,
             answerId,
           },
         },
       }),
     );
-    return deletePreference;
   }
 }

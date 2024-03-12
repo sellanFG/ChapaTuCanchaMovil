5828bd7d20a79df60076633fe96209fd0b88239f
Alonso Bonilla <luis.labp06@gmail.com>
2024-03-11 22:46:46 -0500

    refactor preferences controller

--- src/preferences/preferences.controller.ts
+++ src/preferences/preferences.controller.ts
@@ -1,13 +1,23 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { PreferencesService } from './preferences.service';
 import {
   ApiCreatedResponse,
+  ApiNoContentResponse,
   ApiOkResponse,
   ApiParam,
   ApiTags,
 } from '@nestjs/swagger';
 import { CreatePreferenceDto } from './dto/createPreferenceDto.dto';
-import { PreferencesEntity } from './entities/preferences.entity';
+import { PreferencesEntitySwagger } from './entities/swagger/preferenceSwagger.entity';
+import { PreferencesInterface } from './interfaces/preferences.interface';
 
 @ApiTags('preferences')
 @Controller('preferences')
@@ -15,25 +25,29 @@ export class PreferencesController {
   constructor(private readonly preferencesService: PreferencesService) {}
 
   @Post()
-  @ApiCreatedResponse({ type: PreferencesEntity })
-  create(@Body() createPreferenceDto: CreatePreferenceDto) {
+  @ApiCreatedResponse({ type: [PreferencesEntitySwagger] })
+  create(
+    @Body() createPreferenceDto: CreatePreferenceDto,
+  ): Promise<PreferencesInterface[]> {
     return this.preferencesService.create(createPreferenceDto);
   }
 
   @Get(':id')
   @ApiParam({ name: 'id', type: Number, description: 'Player Id' })
-  @ApiOkResponse({ type: PreferencesEntity })
-  getPreferences(@Param('id') playerId: number): Promise<NonNullable<unknown>> {
+  @ApiOkResponse({ type: [PreferencesEntitySwagger] })
+  getPreferences(
+    @Param('id') playerId: number,
+  ): Promise<PreferencesInterface[]> {
     return this.preferencesService.getPreferences(playerId);
   }
 
-  @Delete(':playerId/:sportId/:answerId')
-  @ApiOkResponse({ type: PreferencesEntity })
-  delete(
+  @Delete(':playerId/:answerId')
+  @HttpCode(204)
+  @ApiNoContentResponse({ description: 'Preference deleted' })
+  async delete(
     @Param('playerId') playerId: number,
-    @Param('sportId') sportId: number,
     @Param('answerId') answerId: number,
-  ) {
-    return this.preferencesService.delete(playerId, sportId, answerId);
+  ): Promise<void> {
+    await this.preferencesService.delete(playerId, answerId);
   }
 }

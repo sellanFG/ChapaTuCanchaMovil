ad51758018b0c441dc01dd5a14a7ad57ad3683cd
Alonso Bonilla <luis.labp06@gmail.com>
2024-03-11 22:43:40 -0500

    fix preference entity

--- src/preferences/entities/preferences.entity.ts
+++ src/preferences/entities/preferences.entity.ts
@@ -1,21 +1,9 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-const arrayPreferences: Record<string, any>[] = [
-  {
-    questionId: 0,
-    question: 'string',
-    answerId: 0,
-    answer: 'string',
-  },
-];
-
-const preferences: Record<string, any> = {
-  sportId: 0,
-  sportImage: 'string',
-  preferences: arrayPreferences,
-};
-
 export class PreferencesEntity {
-  @ApiProperty({ enum: [preferences] })
-  SportName: string;
+  constructor(answerId, playerId) {
+    this.answerId = answerId;
+    this.playerId = playerId;
+  }
+
+  answerId: number;
+  playerId: number;
 }

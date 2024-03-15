5b2a9494ee415cc59d0b8b4d2a08bb5f40ef9770
Alonso Bonilla <luis.labp06@gmail.com>
2024-03-15 13:00:23 -0500

    fix create method and add update method

--- src/preferences/preferences.service.ts
+++ src/preferences/preferences.service.ts
@@ -1,40 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { CreatePreferenceDto } from './dto/createPreferenceDto.dto';
+import { CreatePreferenceDto, UpdatePreferenceDto } from './dto/index';
 import { PreferencesEntity } from './entities/preferences.entity';
 import {
   ArrayPreferences,
   PreferencesInterface,
 } from './interfaces/preferences.interface';
+import { Preference } from './dto/createPreferenceDto.dto';
 
 @Injectable()
 export class PreferencesService {
   constructor(private prisma: PrismaService) {}
 
   async create(data: CreatePreferenceDto): Promise<PreferencesInterface[]> {
-    const { playerId, answerIds } = data;
-    const preferences = answerIds.map(
-      (answerId) => new PreferencesEntity(answerId, playerId),
-    );
-    await this.prisma.handleDbOperation(
-      this.prisma.preferences.createMany({
-        data: preferences,
-      }),
-    );
-    return this.getPreferences(playerId, answerIds);
+    const { playerId, preferences } = data;
+    await this.prisma.preferences.createMany({
+      data: preferences.map(
+        (p) => new PreferencesEntity(p.answerId, playerId, p.sportId),
+      ),
+    });
+    return this.getPreferences(playerId, preferences);
   }
 
   async getPreferences(
     playerId: number,
-    answerIds?: number[],
+    preferences?: Preference[],
   ): Promise<PreferencesInterface[]> {
-    const preferences = await this.prisma.preferences.findMany({
+    const answerIds = preferences?.map((p) => p.answerId);
+    const sportIds = preferences?.map((p) => p.sportId);
+
+    const query = await this.prisma.preferences.findMany({
       where: {
         playerId,
-        Answer: {
-          answerId: {
-            in: answerIds,
-          },
+        answerId: {
+          in: answerIds,
+        },
+        sportId: {
+          in: sportIds,
         },
       },
       include: {
@@ -49,45 +51,46 @@ export class PreferencesService {
         },
       },
     });
-    return this.buildPreferencesModel(preferences);
+    return this.buildPreferencesModel(query);
   }
 
-  private buildPreferencesModel(preferences: any): PreferencesInterface[] {
+  private buildPreferencesModel(query: any): PreferencesInterface[] {
     const model: Record<string, any> = {};
     const sportNames = [];
-    preferences.forEach((preference) => {
-      const sportName = preference.Answer.Question.Sport.sportName;
+    query.forEach((value) => {
+      const sportName = value.Answer.Question.Sport.sportName;
       if (!model[sportName]) {
         model[sportName] = <PreferencesInterface>{
-          sportId: preference.Answer.Question.sportId,
-          sportName: preference.Answer.Question.Sport.sportName,
-          sportImage: preference.Answer.Question.Sport.sportImage,
+          sportId: value.Answer.Question.sportId,
+          sportName: value.Answer.Question.Sport.sportName,
+          sportImage: value.Answer.Question.Sport.sportImage,
           preferences: [],
         };
         sportNames.push(sportName);
       }
 
       model[sportName]['preferences'].push(<ArrayPreferences>{
-        questionId: preference.Answer.questionsTemplateId,
-        question: preference.Answer.Question.question,
-        answerId: preference.answerId,
-        answer: preference.Answer.answer,
+        questionId: value.Answer.questionsTemplateId,
+        question: value.Answer.Question.question,
+        answerId: value.answerId,
+        answer: value.Answer.answer,
       });
     });
 
     return sportNames.map((sportName) => model[sportName]);
   }
 
-  async delete(playerId: number, answerId: number): Promise<void> {
-    await this.prisma.handleDbOperation(
-      this.prisma.preferences.delete({
-        where: {
-          playerId_answerId: {
-            playerId,
-            answerId,
-          },
+  async update(answerId: number, data: UpdatePreferenceDto) {
+    const { playerId, sportId } = data;
+    await this.prisma.preferences.update({
+      where: {
+        playerId_answerId_sportId: {
+          answerId,
+          playerId,
+          sportId,
         },
-      }),
-    );
+      },
+      data,
+    });
   }
 }

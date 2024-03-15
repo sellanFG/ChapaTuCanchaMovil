d4c41acb578d9ad8e1995e23ba923e28a5e039bd
Alonso Bonilla <luis.labp06@gmail.com>
2024-03-15 12:56:15 -0500

    add validation for each preference object

--- src/preferences/dto/createPreferenceDto.dto.ts
+++ src/preferences/dto/createPreferenceDto.dto.ts
@@ -2,10 +2,28 @@ import { ApiProperty } from '@nestjs/swagger';
 import {
   ArrayNotEmpty,
   IsArray,
-  IsInt,
+  IsInstance,
   IsNotEmpty,
   IsNumber,
 } from 'class-validator';
+import { Transform, plainToInstance } from 'class-transformer';
+
+export class Preference {
+  constructor(sportId: number, answerId: number) {
+    this.sportId = sportId;
+    this.answerId = answerId;
+  }
+
+  sportId: number;
+  answerId: number;
+}
+
+const preference: Record<string, any> = [
+  {
+    sportId: 1,
+    answerId: 1,
+  },
+];
 
 export class CreatePreferenceDto {
   @ApiProperty()
@@ -13,10 +31,28 @@ export class CreatePreferenceDto {
   @IsNotEmpty()
   playerId: number;
 
-  @ApiProperty({ type: [Number] })
+  @ApiProperty({ enum: [preference] })
   @IsArray()
   @ArrayNotEmpty()
-  @IsNumber({}, { each: true })
-  @IsInt({ each: true })
-  answerIds: number[];
+  @Transform(({ value }) => {
+    return value.map((v: any) => {
+      const keys = Object.keys(v);
+      if (
+        keys.includes('sportId') &&
+        keys.includes('answerId') &&
+        typeof v['sportId'] === 'number' &&
+        typeof v['answerId'] === 'number'
+      ) {
+        return plainToInstance(Preference, v);
+      } else {
+        return v;
+      }
+    });
+  })
+  @IsInstance(Preference, {
+    each: true,
+    message:
+      'each value in preferences must be an instance of Preference: {sportId: number, answerId: number}',
+  })
+  preferences: Preference[];
 }

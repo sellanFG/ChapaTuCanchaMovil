ff4420faf383125dabcfca98b736b4c49864dfcd
Alonso Bonilla <luis.labp06@gmail.com>
2024-03-15 12:58:36 -0500

    add preference entity

--- src/preferences/entities/preferences.entity.ts
+++ src/preferences/entities/preferences.entity.ts
@@ -1,9 +1,11 @@
 export class PreferencesEntity {
-  constructor(answerId, playerId) {
+  constructor(answerId, playerId, sportId) {
     this.answerId = answerId;
     this.playerId = playerId;
+    this.sportId = sportId;
   }
 
   answerId: number;
   playerId: number;
+  sportId: number;
 }
